14a33a85a0a38fae1ea0131292e2f5dbff2a1f21
leslyto <stefanstoev1993@gmail.com>
2020-08-27 10:32:54 +0300

    Updates

---
+++ precache-manifest.e9f1e8fc38ba556987da10482fdb63a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af25643cbb88219c8eddcc2ea1e8470",
+    "url": "/gpx-route-creator/index.html"
+  },
+  {
+    "revision": "c192210defecc64b1abc",
+    "url": "/gpx-route-creator/static/css/main.7df62d27.chunk.css"
+  },
+  {
+    "revision": "b503f830726235e2a8bb",
+    "url": "/gpx-route-creator/static/js/2.1dffe50c.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/gpx-route-creator/static/js/2.1dffe50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c192210defecc64b1abc",
+    "url": "/gpx-route-creator/static/js/main.087efd9e.chunk.js"
+  },
+  {
+    "revision": "e8dd9eabd35bfbfdb940",
+    "url": "/gpx-route-creator/static/js/runtime-main.e1b80937.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gpx-route-creator/precache-manifest.941f5835a6ab5354622a58f78f96e522.js"
+  "/gpx-route-creator/precache-manifest.e9f1e8fc38ba556987da10482fdb63a7.js"
 );
 
 self.addEventListener('message', (event) => {
